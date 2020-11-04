3c231812b8c26cbfd77d5602af3cc004b1c437c8
mertoz41 <mertozka41@gmail.com>
2020-11-04 14:50:03 -0500

    currentUser projects are shown on list

--- src/components/projects.js
+++ src/components/projects.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import projectStyles from './projects.module.css'
 import {withRouter} from 'react-router'
+import { connect } from 'react-redux'
 
 
 
@@ -22,12 +23,22 @@ class Projects extends Component {
         <div className={projectStyles.container}>
             <h1>All Projects</h1>
             <button onClick={() => this.nuProject()}>New Project</button>
-                <h3 onClick={() => this.toProject('hooper-app')}>Hooper App</h3>
-                <h3 onClick={() => this.toProject('beatbox-bubble-app')}>Beatbox Bubble App</h3>
-                <h3 onClick={() => this.toProject('portfolio')}>Portfolio</h3>
+            {this.props.currentUser.projects.map(project => {
+                return (
+                <h3 onClick={() => this.toProject(`${project.title}`)}>{project.title}</h3>
+                )
+            })}
+               
+                
         </div>
     )
 }
 }
 
-export default withRouter(Projects)
+const mapStateToProps = (state) => {
+    return{
+        currentUser: state.currentUser
+    }
+}
+
+export default connect(mapStateToProps)(withRouter(Projects))
