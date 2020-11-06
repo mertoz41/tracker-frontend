9fdb5dad1851b10f40a2ff59ca399f4133340540
mertoz41 <mertozka41@gmail.com>
2020-11-05 22:08:43 -0500

    project info edit success with a little bug

--- src/components/projectpage/projectinfo.js
+++ src/components/projectpage/projectinfo.js
@@ -5,21 +5,50 @@ import store from '../../redux/store'
 
 class Projectinfo extends Component {
     state ={
-        description: ""
+        description: "",
+        edit: false,
+        editing: this.props.shownProject.description
     }
 
-    fixState = (e) =>{
+    fixDescription = (e) =>{
         this.setState({description: e.target.value})
     }
 
-    addDesc = (e) =>{
+    // addDesc = (e) =>{
+    //     // e.preventDefault()
+    //     // fetch(`http://localhost:3000/projects/${this.props.shownProject.id}`,{
+    //     //     method: "PATCH",
+    //     //     headers: {
+    //     //         "Content-Type": "application/json"
+    //     //     },
+    //     //     body: JSON.stringify(this.state)
+    //     // })
+    //     // .then(resp => resp.json())
+    //     // .then(resp => {
+    //     //     let updatedProject = resp.project
+    //     //     let userProjects = store.getState().currentUser.projects
+    //     //     let updatedUser = store.getState().currentUser
+    //     //     let filtered = userProjects.filter(project => project.id !== resp.project.id)
+    //     //     filtered.push(updatedProject)
+    //     //     updatedUser.projects = filtered 
+    //     //     store.dispatch({type: "ADD_DESCRIPTION", shownProject: updatedProject})
+    //     //     store.dispatch({type: "UPDATE_ALL_PROJECTS", currentUser: updatedUser})
+    //     // })
+        
+
+    // }
+    edit = (e, desc) =>{
         e.preventDefault()
+        let obj ={
+            description: desc
+        }
+
         fetch(`http://localhost:3000/projects/${this.props.shownProject.id}`,{
             method: "PATCH",
             headers: {
                 "Content-Type": "application/json"
             },
-            body: JSON.stringify(this.state)
+            body: JSON.stringify(obj)
         })
         .then(resp => resp.json())
         .then(resp => {
@@ -32,22 +61,41 @@ class Projectinfo extends Component {
             store.dispatch({type: "ADD_DESCRIPTION", shownProject: updatedProject})
             store.dispatch({type: "UPDATE_ALL_PROJECTS", currentUser: updatedUser})
         })
-        
+        if (this.state.edit){
+            this.setState({edit: false})
+        }
 
     }
+
+    fixEdit = (e) =>{
+        this.setState({editing: e.target.value})
+    }
+
+   
 
     render() {
         return (
             <div className={infoStyles.container}>
                 <h1>project info experiment</h1>
                 {this.props.shownProject.description ?
-            <p>{this.props.shownProject.description}</p>
+                <div>
+                    <p>{this.props.shownProject.description}</p>
+                    {this.state.edit ?
+                    <div>
+                    <textarea onChange={(e) => this.fixEdit(e)} value={this.state.editing}/>
+                    <button onClick={(e) => this.edit(e, this.state.editing)}>Submit</button>
+                    </div>
+                    :
+                    <button onClick={() => this.setState({edit: true})}>Edit</button>
+                    }
+
+                </div>
             :
             <div>
 
             <p>Add a description for this project.</p>
-            <textarea onChange={(e) => this.fixState(e)} value={this.state.description} placeholder="project description" rows="5" cols="30"/>
-            <button onClick={(e) => this.addDesc(e)}>Add</button>
+            <textarea onChange={(e) => this.fixDescription(e)} value={this.state.description} placeholder="project description" rows="5" cols="30"/>
+            <button onClick={(e) => this.edit(e, this.state.description)}>Add</button>
             </div>
             }
 
