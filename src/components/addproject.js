137816bb7d52c39f116bb0ff787fbe6aa3a66ddd
mertoz41 <mertozka41@gmail.com>
2020-11-12 21:22:45 -0500

    homepage layout enhanced

--- src/components/addproject.js
+++ src/components/addproject.js
@@ -1,12 +1,35 @@
-import React from 'react'
+import React, { Component } from 'react'
 import addProjectStyles from './addproject.module.css'
+import Questionnaire from '../components/newproject/questionnaire'
 
-export default function AddProject() {
+class AddProject extends Component {
+    state={
+        showingQuestions: false
+    }
+
+    
+
+
+
+    render(){
     return (
         <div className={addProjectStyles.container}>
-            <h1>ADD NEW PROJECT</h1>
-            <p>WHERE YOULL ADD A NEW PROJECT</p>
+            {this.state.showingQuestions ?
+            <Questionnaire />
+            :
+            <div className={addProjectStyles.plus} onClick={() => this.setState({showingQuestions: !this.state.showingQuestions})}>
+                <h1 className={addProjectStyles.add}>+</h1>
+            </div>
+            }   
+            <div className={addProjectStyles.words}>
+                <h1 className={addProjectStyles.word}>NEW PROJECT</h1>
+
+            </div>
             
         </div>
     )
+    }
 }
+
+
+export default AddProject
