a65789f6271279db57f9ec87cdc906eff59b5f09
mertoz41 <mertozka41@gmail.com>
2020-11-17 04:07:58 -0500

    delete button executes

--- src/components/projectcard.js
+++ src/components/projectcard.js
@@ -1,23 +1,41 @@
 import React, { Component } from 'react'
 import projectStyles from './projects.module.css'
 import store from '../redux/store'
+import {withRouter} from 'react-router'
+
 
 export class Projectcard extends Component {
     state = {
         isHovering: false
     }
     toProject = (project) =>{
+         
         let projects = store.getState().userProjects
-        let foundProject = projects.find(proj => proj.title == project)
+        let foundProject = projects.find(proj => proj.title == project.title)
         store.dispatch({type: "SHOW_PROJECT", shownProject: foundProject})
         this.props.history.push(`/projects/${project}`)
     }
+
+    deleteProject = (project) => {
+        fetch(`http://localhost:3000/projects/${project.id}`, {
+            method: "DELETE"
+        })
+        .then(resp => resp.json())
+        .then(resp => {
+            let userProjects = store.getState().userProjects
+            let filtered = userProjects.filter(proj => proj.id !== resp.deleted_project.id)
+            let updatedProjects = [...filtered]
+            store.dispatch({type: "UPDATE_ALL_PROJECTS", userProjects: updatedProjects})
+            // only userProjects handled, not projects in currentuser 
+        })
+
+    }
     render() {
         return (
-            <div className={projectStyles.card} onClick={() => this.toProject(`${this.props.project.title}`)} onMouseEnter={() => this.setState({isHovering: true})} onMouseLeave={() => this.setState({isHovering: false})}>
-                        <h3>{this.props.project.title}</h3>
+            <div className={projectStyles.card}  onMouseEnter={() => this.setState({isHovering: true})} onMouseLeave={() => this.setState({isHovering: false})}>
+                        <h3 onClick={() => this.toProject(this.props.project)}>{this.props.project.title} </h3>
                         {this.state.isHovering ?
-                        <button>erase</button>
+                        <button onClick={() => this.deleteProject(this.props.project)}>erase</button>
                         :
                         null   
                         }
@@ -28,4 +46,4 @@ export class Projectcard extends Component {
     }
 }
 
-export default Projectcard
+export default withRouter(Projectcard)
